f84bd2ed3a92d90d345eb0890414b46fe7c693e1
Kaizan08 <jlsnyder08@gmail.com>
2017-06-15 18:19:42 -0500

    Finished getting value onsubmit

--- main.js
+++ main.js
@@ -3,17 +3,21 @@
 */
 
 // 1. First select and store the elements you'll be working with
-var musicdiv = document.querySelector('.player');
-var soundcontrol = document.querySelector('.music-player');
-var search = document.querySelector('.search');
-var search_form = document.querySelector('.search_form');
-var results = document.querySelector('.results');
-var submit = document.querySelector('.submit');
+var musicdiv = document.querySelector(".player");
+var soundcontrol = document.querySelector(".music-player");
+var search = document.querySelector(".search_input");
+var search_form = document.querySelector(".search_form");
+var results = document.querySelector(".results");
+var submit = document.querySelector("#submit");
+
 // 2. Create your `onSubmit` event for getting the user's search term
+var url = "http://api.soundcloud.com/tracks/";
+var client_id = '?client_id=8538a1744a7fdaa59981232897501e04';
 function sendData(){
-  
-  
+  url += client_id + '&q='+ search.value; 
+  console.log(url);
 };
+
 
 // 3. Create your `fetch` request that is called after a submission
 
