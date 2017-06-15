d0c3941d7d5bd28a72a647a58a8e4fdf4818675a
Kaizan08 <jlsnyder08@gmail.com>
2017-06-15 12:02:01 -0500

    Soundcloud first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+/*
+  Here is a guide for the steps you could take:
+*/
+
+// 1. First select and store the elements you'll be working with
+var musicdiv = document.querySelector('.player');
+var soundcontrol = document.querySelector('.music-player');
+var search = document.querySelector('.search');
+var search_form = document.querySelector('.search_form');
+var results = document.querySelector('.results');
+var submit = document.querySelector('.submit');
+// 2. Create your `onSubmit` event for getting the user's search term
+function sendData(){
+  
+  
+};
+
+// 3. Create your `fetch` request that is called after a submission
+
+
+// 4. Create a way to append the fetch results to your page
+
+
+// 5. Create a way to listen for a click that will play the song in the audio play
