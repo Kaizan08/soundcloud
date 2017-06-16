fc8bbaf8b6f071e3b5849ccdde95c2ca8bb4721a
Kaizan08 <jlsnyder08@gmail.com>
2017-06-16 14:02:45 -0500

    function working for pulling data back the way I expect it to

--- main.js
+++ main.js
@@ -11,18 +11,41 @@ var results = document.querySelector(".results");
 var submit = document.querySelector("#submit");
 
 // 2. Create your `onSubmit` event for getting the user's search term
-var url = "http://api.soundcloud.com/tracks/";
-var client_id = '?client_id=8538a1744a7fdaa59981232897501e04';
-function sendData(){
-  url += client_id + '&q='+ search.value; 
-  console.log(url);
-};
+//http://api.soundcloud.com/users/52955/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f
+//var url = "http://api.soundcloud.com/tracks/";
+var url = "http://api.soundcloud.com/users/"
+var client_id = "?client_id=8538a1744a7fdaa59981232897501e04";
+function sendData() {
+  var urltosearch = search.value;
+  url += urltosearch +'/' + client_id ;
+  axios.get(url).then(function(data) {
+    userId = data["data"]["id"];
+    getTracks(userId);
+  });
+}
 
-
+function getTracks(userId){
+  var trackurl = "http://api.soundcloud.com/users/";
+  trackurl += userId + '/tracks' + client_id;
+  axios.get(trackurl).then(function(data){
+    pushToPage(data);
+  });
+}
 // 3. Create your `fetch` request that is called after a submission
 
+function pushToPage(data) {
+  // automatically load first song after search completes
+  // soundcontrol.src = data['data'][0]['stream_url'] + client_id;
+  // soundcontrol.autoplay = true;
+  console.log(data);
+  for (var i = 0; i < data.length; i++) {
+    buildoutput(output[i]);
+  }
+}
 
 // 4. Create a way to append the fetch results to your page
+function buildoutput(data){
 
-
+  
+}
 // 5. Create a way to listen for a click that will play the song in the audio play
