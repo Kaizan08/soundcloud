c1eb6bff95b6851ef92f54991a4706638b5df398
Kaizan08 <jlsnyder08@gmail.com>
2017-06-16 16:57:47 -0500

    few alterations

--- main.js
+++ main.js
@@ -9,13 +9,14 @@ var search = document.querySelector(".search_input");
 var search_form = document.querySelector(".search_form");
 var results = document.querySelector(".results");
 var submit = document.querySelector("#submit");
-
 // 2. Create your `onSubmit` event for getting the user's search term
 //http://api.soundcloud.com/users/52955/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f
 //var url = "http://api.soundcloud.com/tracks/";
 
 var client_id = "?client_id=8538a1744a7fdaa59981232897501e04";
 function sendData() {
+  // var removal = document.querySelector('music_prof');
+  // results.removeChild(removal);
   var url = "http://api.soundcloud.com/users/"
   var urltosearch = search.value;
   url += urltosearch +'/' + client_id ;
@@ -38,27 +39,31 @@ function pushToPage(data) {
   // automatically load first song after search completes
   // soundcontrol.src = data['data'][0]['stream_url'] + client_id;
   // soundcontrol.autoplay = true;
-  console.log(data);
   var h2element = document.createElement('h2');
   h2element.textContent = 'Search Results:';
   h2element.style.fontWeight = 'bold';
   results.appendChild(h2element);
   var bottomdiv = document.createElement('div');
-  bottomdiv.ClassList.add('bottomresults');
+  bottomdiv.classList.add('bottomresults');
   results.appendChild(bottomdiv);
-  for (var i = 0; i < data.length; i++) {
-    buildoutput(output[i], bottomdiv);
+  for (var i = 0; i < data["data"].length; i++) {
+    buildprofile(data["data"][i], bottomdiv);
   }
 }
 
 // 4. Create a way to append the fetch results to your page
-function buildoutput(data, parent){
+function buildprofile(data, parent){
   var musicdiv = document.createElement('div');
-  musicdiv.ClassList = 'music_prof';
+  musicdiv.classList.add('music_prof');
   parent.appendChild(musicdiv);
   var img = document.createElement('img');
-  img.src = data["data"]["artwork_url"];
+  img.src = data["artwork_url"];
   musicdiv.appendChild(img);
-  
+  var title = document.createElement('p');
+  title.textContent = data["title"];
+  musicdiv.appendChild(title);
+  var band = document.createElement('p');
+  band.textContent = data["username"];
+  musicdiv.appendChild(band);
 }
 // 5. Create a way to listen for a click that will play the song in the audio play
