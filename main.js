4e7bca791843133d48567aa01aea770559b947db
Kaizan08 <jlsnyder08@gmail.com>
2017-06-16 15:10:26 -0500

    updates to pulling

--- main.js
+++ main.js
@@ -13,9 +13,10 @@ var submit = document.querySelector("#submit");
 // 2. Create your `onSubmit` event for getting the user's search term
 //http://api.soundcloud.com/users/52955/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f
 //var url = "http://api.soundcloud.com/tracks/";
-var url = "http://api.soundcloud.com/users/"
+
 var client_id = "?client_id=8538a1744a7fdaa59981232897501e04";
 function sendData() {
+  var url = "http://api.soundcloud.com/users/"
   var urltosearch = search.value;
   url += urltosearch +'/' + client_id ;
   axios.get(url).then(function(data) {
@@ -38,14 +39,26 @@ function pushToPage(data) {
   // soundcontrol.src = data['data'][0]['stream_url'] + client_id;
   // soundcontrol.autoplay = true;
   console.log(data);
+  var h2element = document.createElement('h2');
+  h2element.textContent = 'Search Results:';
+  h2element.style.fontWeight = 'bold';
+  results.appendChild(h2element);
+  var bottomdiv = document.createElement('div');
+  bottomdiv.ClassList.add('bottomresults');
+  results.appendChild(bottomdiv);
   for (var i = 0; i < data.length; i++) {
-    buildoutput(output[i]);
+    buildoutput(output[i], bottomdiv);
   }
 }
 
 // 4. Create a way to append the fetch results to your page
-function buildoutput(data){
-
+function buildoutput(data, parent){
+  var musicdiv = document.createElement('div');
+  musicdiv.ClassList = 'music_prof';
+  parent.appendChild(musicdiv);
+  var img = document.createElement('img');
+  img.src = data["data"]["artwork_url"];
+  musicdiv.appendChild(img);
   
 }
 // 5. Create a way to listen for a click that will play the song in the audio play
